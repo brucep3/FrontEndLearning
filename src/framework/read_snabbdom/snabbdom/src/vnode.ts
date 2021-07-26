8161e5739c393e8c218e3ff22474ffe734a1e5b4
Bruce Pan <brucepan3@163.com>
2021-07-26 17:14:52 +0800

    Update

---
+++ src/framework/read_snabbdom/snabbdom/src/vnode.ts
@@ -0,0 +1,47 @@
+import { Hooks } from "./hooks";
+import { AttachData } from "./helpers/attachto";
+import { VNodeStyle } from "./modules/style";
+import { On } from "./modules/eventlisteners";
+import { Attrs } from "./modules/attributes";
+import { Classes } from "./modules/class";
+import { Props } from "./modules/props";
+import { Dataset } from "./modules/dataset";
+
+export type Key = string | number | symbol;
+
+export interface VNode {
+  sel: string | undefined; // selector 的缩写
+  data: VNodeData | undefined; // 下面 VNodeData 接口的内容
+  children: Array<VNode | string> | undefined;
+  elm: Node | undefined; // element的缩写，存储了真实的HTMLElement
+  text: string | undefined; // 如果是文本节点，则存储text
+  key: Key | undefined; // 节点的key，在做列表时很有用
+}
+
+export interface VNodeData {
+  props?: Props;
+  attrs?: Attrs;
+  class?: Classes;
+  style?: VNodeStyle;
+  dataset?: Dataset;
+  on?: On;
+  attachData?: AttachData;
+  hook?: Hooks;
+  key?: Key;
+  ns?: string; // for SVGs
+  fn?: () => VNode; // for thunks
+  args?: any[]; // for thunks
+  is?: string; // for custom elements v1
+  [key: string]: any; // for any other 3rd party module
+}
+
+export function vnode(
+  sel: string | undefined,
+  data: any | undefined,
+  children: Array<VNode | string> | undefined,
+  text: string | undefined,
+  elm: Element | Text | undefined
+): VNode {
+  const key = data === undefined ? undefined : data.key;
+  return { sel, data, children, text, elm, key };
+}
